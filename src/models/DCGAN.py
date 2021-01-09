eb00d1df8d4f829ac78c61339060dccaac323d95
Udbhav Prasad <32470339+UdbhavPrasad072300@users.noreply.github.com>
2021-01-09 11:46:19 -0500

    DCGAN training, retraining GAN, bug fixes

--- src/models/DCGAN.py
+++ src/models/DCGAN.py
@@ -4,36 +4,22 @@ import torch.nn.functional as F
 
 
 class DCGAN_Generator(nn.Module):
-    def __init__(self, noise_size, out_channel, hidden_size, num_layers=1, kernel_size=3, stride=2, factor=1):
+    def __init__(self, noise_size, out_channel, hidden_size):
         super(DCGAN_Generator, self).__init__()
 
-        assert num_layers >= 1
-
-        self.num_layers = num_layers
-        self.kernel_size = kernel_size
-        self.stride = stride
-
-        self.generator = []
-
-        for index in range(self.num_layers):
-            if index==0:
-                self.generator.append(
-                    self.get_generator_block(noise_size, hidden_size, kernel_size=kernel_size, stride=stride)
-                )
-            else:
-                self.generator.append(
-                    self.get_generator_block(int(hidden_size / factor), int(hidden_size / (factor * 2)), kernel_size=kernel_size,
-                                             stride=stride)
-                )
-                factor = factor * 2
+        self.noise_size = noise_size
+        self.hidden_size = hidden_size
 
         self.generator = nn.Sequential(
-            *self.generator,
-            nn.ConvTranspose2d(int(hidden_size / factor), out_channel, kernel_size=kernel_size, stride=stride),
+            self.get_generator_block(noise_size, hidden_size * 4),
+            self.get_generator_block(hidden_size * 4, hidden_size * 2, kernel_size=4, stride=1),
+            self.get_generator_block(hidden_size * 2, hidden_size),
+            nn.ConvTranspose2d(hidden_size, out_channel, kernel_size=4, stride=2),
             nn.Tanh()
         )
 
     def forward(self, noise):
+        noise = noise.view(noise.size(0), self.noise_size, 1, 1)
         return self.generator(noise)
 
     def get_generator_block(self, in_channels, out_channels, kernel_size=3, stride=2):
@@ -42,32 +28,17 @@ class DCGAN_Generator(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.ReLU()
         )
+        
         return block
 
 class DCGAN_Discriminator(nn.Module):
-    def __init__(self, in_size, hidden_size, num_layers=1, kernel_size=4, stride=2, factor=1):
+    def __init__(self, in_size, hidden_size):
         super(DCGAN_Discriminator, self).__init__()
 
-        assert num_layers >= 1
-
-        self.num_layers = num_layers
-
-        self.discriminator = []
-
-        for index in range(self.num_layers):
-            if index==0:
-                self.discriminator.append(
-                    self.get_discriminator_block(in_size, hidden_size, kernel_size, stride)
-                )
-            else:
-                self.discriminator.append(
-                    self.get_discriminator_block(hidden_size * factor, hidden_size * factor * 2, kernel_size, stride)
-                )
-                factor = factor * 2
-
         self.discriminator = nn.Sequential(
-            *self.discriminator,
-            nn.Conv2d(hidden_size * factor, 1, kernel_size=kernel_size, stride=stride)
+            self.get_discriminator_block(in_size, hidden_size),
+            self.get_discriminator_block(hidden_size, hidden_size * 2),
+            nn.Conv2d(hidden_size * 2, 1, kernel_size=4, stride=2)
         )
 
     def forward(self, image):
