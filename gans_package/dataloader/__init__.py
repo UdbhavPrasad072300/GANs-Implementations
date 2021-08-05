281e7868fc384d3ae947237a5c3d44426772ab4e
Udbhav Prasad <udbhavprasad072300@gmail.com>
2021-08-05 13:05:07 -0400

    cityscape dataloader init

---
+++ gans_package/dataloader/__init__.py
@@ -0,0 +1,1 @@
+from gans_package.dataloader.cityscape import CityScapeDataset
---
+++ gans_package/dataloader/cityscape.py
@@ -0,0 +1,13 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class CityScapeDataset(Dataset):
+    def __init__(self, data_path):
+        super(CityScapeDataset, self).__init__()
+
+    def __getitem__(self, item):
+        return
+
+    def __len__(self):
+        return
